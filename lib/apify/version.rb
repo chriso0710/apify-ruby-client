a062d074ad4928ecd81b062db6efc14c46794e3a
paresharma <paresharma@gmail.com>
2018-05-28 23:24:47 +0900

    Initialize

---
+++ lib/apify/version.rb
@@ -0,0 +1,3 @@
+module Apify
+  VERSION = "0.1.0"
+end
---
+++ spec/apify_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Apify do
+  it "has a version number" do
+    expect(Apify::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
